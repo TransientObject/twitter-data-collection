e1f8789f71bb610089afe3a111a48181aa05e9ef
Priya PNS <mail2priyanarayanan@gmail.com>
2018-01-29 23:31:25 -0800

    Adding coordinates of the 30 cities

--- base.py
+++ base.py
@@ -59,5 +59,19 @@ if __name__ == '__main__':
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     create_table_if_not_exists()
     TweetInfo.init_connection_to_db(DBNAME)
-    for tup in [("40.7128,-74.0060,20km", "NYC"),("4.7128,-7.0060,20km", "LA"),("80.7128,-4.0060,20km", "ORLANDO")]:
+    for tup in [("40.7128,-74.0060,20km", "NYC"),("39.9526,-75.1652,20km", "Philadelphia"),
+                ("21.4389,-158.0001,20km","Honolulu - Oahu"),("20.7984,-156.3319,20km","Maui"),
+                ("37.7749,-122.4194,20km","San Francisco"),("36.0544,-112.1401,20km","Grand Canyon"),
+                ("38.2919,-122.4580,20km","Sonoma"),("38.9072,-77.0369,20km","Washington D.C"),
+                ("29.9511,-90.0715,20km","New Orleans"),("32.7157,-117.1611,20km","San Diego"),
+                ("22.0964,-159.5261,20km","Kauai"),("19.8968,-155.5828,20km","Hawaii"),
+                ("39.4817,-106.0384,20km","Breckenridge"),("42.3601,-71.0589,20km","Boston"),
+                ("25.7617,-80.1918,20km","Miami"),("34.0522,-118.2437,20km","Los Angeles"),
+                ("32.7765,-79.9311,20km","Charleston"),("37.8651,-119.5383,20km","Yosemite"),
+                ("47.6062,-122.3321,20km","Seattle"),("35.5951,-82.5515,20km","Asheville, NC"),
+                ("40.6461,-111.4980,20km","Park City, Utah"),("39.7392,-104.9903,20km","Denver"),
+                ("32.0835,-81.0998,20km","Savannah"),("36.3615,-121.8563,20km","Big Sur"),
+                ("44.4280,-110.5885,20km","Yellowstone"),("41.8781,-87.6298", "Chicago"),
+                ("36.1699,-115.1398", "Las Vegas"),("45.512794,-122.679565,20km","Portland"),
+                ("39.8309°,-77.2311,20km", "Gettysburg"),("37.8651,-119.5383,20km","Yosemite")]:
         store_travel_tweets_from_location(tup[0], tup[1])
