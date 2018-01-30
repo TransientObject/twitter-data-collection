535a4eab10d057f4ed131da23c46aafd175aba6b
Priya PNS <mail2priyanarayanan@gmail.com>
2018-01-29 16:55:54 -0800

    Using Max Id to stop pulling redundant tweets. Pulling recent 10000 tweets

--- TweetInfo.py
+++ TweetInfo.py
@@ -5,9 +5,10 @@ class TweetInfo():
     conn = -1
 
     # Data on the tweet
-    def __init__(self, text, user, date, tweet_location, search_location, tweet_id, tweet_json):
+    def __init__(self, text, user, user_id, date, tweet_location, search_location, tweet_id, tweet_json):
         self.tweetText = text
         self.user_profile = user
+        self.user_id = user_id
         self.tweet_id = tweet_id
         self.date = date
         self.tweet_location = tweet_location
@@ -21,5 +22,8 @@ class TweetInfo():
 
     # Inserting that data into the DB
     def insertTweet(self):
-        TweetInfo.c.execute("INSERT INTO tweets (tweetText, user_profile, date, tweet_location, search_location, tweet_id, tweet_json) VALUES (?, ?, ?, ?, ?, ?, ?)", (self.tweetText, self.user_profile, self.date, self.tweet_location, self.search_location, self.tweet_id, self.tweet_json))
-        TweetInfo.conn.commit()
+        try:
+            TweetInfo.c.execute("INSERT INTO tweets (tweetText, user_profile, user_id, date, tweet_location, search_location, tweet_id, tweet_json) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (self.tweetText, self.user_profile, self.user_id, self.date, self.tweet_location, self.search_location, self.tweet_id, self.tweet_json))
+            TweetInfo.conn.commit()
+        except Exception as e:
+            print("Exception while inserting into db - " + str(e))
--- base.py
+++ base.py
@@ -14,21 +14,38 @@ def create_table_if_not_exists():
     c.execute('''CREATE TABLE IF NOT EXISTS tweets
         (tweetText text,
         user_profile text,
+        user_id text,
         date text,
         tweet_location text,
         search_location text,
-        tweet_id text,
+        tweet_id text PRIMARY KEY,
         tweet_json text)''')
     conn.commit()
     conn.close()
 
+def get_max_id_for_location(location):
+    conn = sqlite3.connect(DBNAME)
+    c = conn.cursor()
+    c.execute("SELECT min(tweet_id) FROM tweets where search_location = ?", [location])
+    rows = c.fetchall()
+    return rows[0][0]
+    conn.close()
+
+
 def store_travel_tweets_from_location(coordinates, geotag):
     try:
-        travelTweets = tweepy.Cursor(api.search, q='ttot', geocode=coordinates).items(10)
+        max_id = get_max_id_for_location(geotag)
+        item_count = 10000
+        if max_id is None:
+            travelTweets = tweepy.Cursor(api.search, q='ttot', geocode=coordinates).items(item_count)
+        else:
+            max_id = str(int(max_id) - 1)
+            travelTweets = tweepy.Cursor(api.search, q='ttot', geocode=coordinates, max_id = max_id).items(item_count)
+
         for tweet in travelTweets:
             status_string = json.dumps(tweet._json)
             status_json = json.loads(status_string)
-            tweet_info = TweetInfo(status_json['text'], status_json['user']['screen_name'], status_json['created_at'], status_json['user']['location'], geotag, status_json['id'], status_string)
+            tweet_info = TweetInfo(status_json['text'], 'https://twitter.com/' + status_json['user']['screen_name'], status_json['user']['id_str'], status_json['created_at'], status_json['user']['location'], geotag, status_json['id'], status_string)
             tweet_info.insertTweet()
     except Exception as e:
         print("Exception Message - {}".format(str(e)))
